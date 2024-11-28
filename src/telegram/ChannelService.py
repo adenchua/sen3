c39dbedb6845e6462dacf39e405dc22327b4670c
Aden Chua <37574101+adenchua@users.noreply.github.com>
2024-11-28 14:31:32 +0800

    add methods get_channel_messages and get_recommended_channels

--- src/telegram/ChannelService.py
+++ src/telegram/ChannelService.py
@@ -1,14 +1,20 @@
 from telethon import TelegramClient, functions
-from telethon.tl.types import messages
+from telethon.tl.types import messages, Message, MessageService
+
+from typing import Optional, List
 
 
 class ChannelService:
+    """
+    Telegram wrapper class that provides telegram channel related methods
+    """
+
     def __init__(self, api_id: int, api_hash: str):
         self.telegram_client: TelegramClient = TelegramClient("anon", api_id, api_hash)
 
     async def get_channel(self, channel_id: str):
         """
-        Given a channel id (chat username), return a telegram Channel object
+        Returns a telegram Channel object
         """
         try:
             client: TelegramClient
@@ -31,12 +37,59 @@ class ChannelService:
         except Exception as error:
             print(f"Error: {error}")
 
-    async def get_channel_messages(self, channel_id: str):
-        pass
+    async def get_channel_messages(
+        self,
+        channel_id: str,
+        offset_id: Optional[int] = 0,
+        reverse: Optional[bool] = True,
+        limit: Optional[int] = 100,
+    ):
+        """
+        Fetches messages from a channel.
+
+        If an offset_id is provided, messages will be fetched from that offset id.
+        e.g. if offset_id of 0 is provided with a limit of 3, messages of id 1,2,3 will be returned
+
+        To obtain the latest messages from a channel, set the reverse parameter to False.
+        """
+        try:
+            client: TelegramClient
+            async with self.telegram_client as client:
+                response: List[Message | MessageService] = await client.get_messages(
+                    entity=channel_id,
+                    limit=limit,
+                    offset_id=offset_id,
+                    reverse=reverse,
+                )
+
+                result = []
+
+                # process the response
+                # filter non Messages such as announcements/channel edit
+                # parse message
+                for message in response:
+                    # MessageService is returned by the client in the array, need to filter out
+                    if isinstance(message, Message):
+                        result.append(
+                            {
+                                "channel_id": channel_id,
+                                "id": message.id,
+                                "date": message.date,
+                                "message": message.message,
+                                "view_count": message.views,
+                                "forward_count": message.forwards,
+                                "reply_count": message.replies,
+                                "edit_date": message.edit_date,
+                            }
+                        )
+
+                return result
+        except Exception as error:
+            print(f"Error: {error}")
 
     async def channel_exists(self, channel_id: str) -> bool:
         """
-        Given a channel id, return True if the channel is valid
+        Returns True if a telegram channel is valid
         """
         try:
             client: TelegramClient
@@ -50,5 +103,26 @@ class ChannelService:
         except Exception as error:
             print(f"Error: {error}")
 
-    async def get_recommended_channels(self, channel_id: str):
-        pass
+    async def get_recommended_channels(self, channel_id: str) -> List[str]:
+        """
+        Returns a list of recommended channel ids for a given channel
+
+        Recommendations are given by telegram and usually 10 channels will be recommended
+        """
+        try:
+            client: TelegramClient
+            async with self.telegram_client as client:
+                response: messages.Chats = await client(
+                    functions.channels.GetChannelRecommendationsRequest(
+                        channel=channel_id
+                    )
+                )
+
+                result: List[str] = []
+
+                for channel in response.chats:
+                    result.append(channel.username)
+
+                return result
+        except Exception as error:
+            print(f"Error: {error}")
