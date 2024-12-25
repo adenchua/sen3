606b26692896de38b5b5de29543f9d92dbde2b74
Aden Chua <37574101+adenchua@users.noreply.github.com>
2024-12-25 11:38:42 +0800

    update packages

--- backend/src/controllers/telegram-controllers/getChat.ts
+++ backend/src/controllers/telegram-controllers/getChat.ts
@@ -2,12 +2,8 @@ import { Request, Response } from "express";
 
 import { telegramInstance } from "../../singletons";
 
-interface RequestParams {
-  chatUsername: string;
-}
-
 export default async function getChat(request: Request, response: Response): Promise<void> {
-  const { chatUsername } = request.params as unknown as RequestParams;
+  const { chatUsername } = request.params;
 
   const chat = await telegramInstance.fetchChat(chatUsername);
 
