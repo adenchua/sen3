1dd7318c24ad4adc9658f13d202e2152ffbb0b21
Aden Chua <37574101+adenchua@users.noreply.github.com>
2025-05-10 10:01:12 +0800

    fix chart width to be responsive to the container

--- frontend/src/components/charts/LineChart.tsx
+++ frontend/src/components/charts/LineChart.tsx
@@ -6,6 +6,7 @@ import {
   Tooltip,
   XAxis,
   YAxis,
+  ResponsiveContainer,
 } from "recharts";
 import { orange } from "@mui/material/colors";
 
@@ -21,25 +22,27 @@ interface IProps {
 }
 
 export default function LineChart(props: IProps) {
-  const { data, lineKeys, height = 300, width = 500 } = props;
+  const { data, lineKeys, height = 300, width = "100%" } = props;
 
   return (
-    <RechartsLineChart width={width} height={height} data={data} margin={{ bottom: 30 }}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" angle={-45} textAnchor="end" />
-      <YAxis />
-      <Tooltip />
-      <Legend verticalAlign="top" align="center" />
-      {lineKeys.map((key) => (
-        <Line
-          key={key}
-          type="monotone"
-          dataKey={key}
-          stroke={orange[800]}
-          strokeWidth={2}
-          dot={false}
-        />
-      ))}
-    </RechartsLineChart>
+    <ResponsiveContainer width={width} height={height}>
+      <RechartsLineChart data={data} margin={{ bottom: 30, right: 30 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" angle={-45} textAnchor="end" />
+        <YAxis />
+        <Tooltip />
+        <Legend verticalAlign="top" align="center" />
+        {lineKeys.map((key) => (
+          <Line
+            key={key}
+            type="monotone"
+            dataKey={key}
+            stroke={orange[800]}
+            strokeWidth={2}
+            dot={false}
+          />
+        ))}
+      </RechartsLineChart>
+    </ResponsiveContainer>
   );
 }
